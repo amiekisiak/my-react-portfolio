dc9c2085bbd89694471bc6a13d1cc648e585e90d
amiekisiak <117371691+amiekisiak@users.noreply.github.com>
2023-03-06 22:40:57 +0000

    updated background photo

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ import About from './pages/About';
 import Navbar from './components/NavBar';
 import Footer from './components/Footer';
 import Wrapper from './components/Wrapper';
-import "./App.css";
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import background from './assets/img/gradienta-QWutu2BRpOs-unsplash.jpg';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Wrapper>
-          <Routes>
-            <Route path="/" element={<About/>} />
-            <Route path="/about" element={<About/>} />
-          </Routes>
-        </Wrapper>
-        <Footer />
-      </div>
-    </Router>
+    <div style={{ backgroundImage: `url(${background})` }}>
+      Hi
+      <Router>
+        <div>
+          <Navbar />
+          <Wrapper>
+            <Routes>
+              <Route path="/" element={<About />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </Wrapper>
+          <Footer />
+        </div>
+      </Router>
+    </div>
   );
 }
 
