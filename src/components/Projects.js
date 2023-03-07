44f2412fb10fce9b97c27bcf3700b0fecbdb15cf
amiekisiak <117371691+amiekisiak@users.noreply.github.com>
2023-03-07 10:19:18 +0000

    added more images for cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import { ProjectCard } from "./ProjectCard";
 import "./App.css";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import projImg1 from "../assets/img/projImg1.jpg";
 
 
 export const Projects = () => {
