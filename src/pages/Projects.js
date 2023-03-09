928022bb42727bfef9874d16797d010bc5a391bc
amiekisiak <117371691+amiekisiak@users.noreply.github.com>
2023-03-09 19:53:27 +0000

    added file

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,6 @@
-import { Container, Row,Tab, Col, Nav } from "react-bootstrap";
+
 import { ProjectCard } from "../components/ProjectCard";
+import { Container, Row,Tab, Col, Nav } from "react-bootstrap";
 import TrackVisibility from 'react-on-screen';
 import projImg1 from "../assets/img/img_projImg1.png";
 import projImg2 from "../assets/img/img_projImg2.png";
@@ -11,77 +12,67 @@ import projImg6 from "../assets/img/img_projImg6.png";
 
 export const Projects = () => {
 
-    const projects = [
-      {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg1,
-      },
-      {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg2,
-      },
-      {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg3,
-      },
-      {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg4,
-      },
-      {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg5,
-      },
-      {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg6,
-      },
-    ];
-  
-    return (
-      <section className="project" id="project">
-        <Container>
-          <Row>
-            <Col size={12}>
-              <TrackVisibility>
-                {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                  <h2>Projects</h2>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                      <Nav.Item>
-                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                      </Nav.Item>
-                      <Nav.Item>
-                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                      </Nav.Item>
-                      <Nav.Item>
-                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                      </Nav.Item>
-                    </Nav>
-                    <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                      <Tab.Pane eventKey="first">
-                        <Row>
-                          {
-                            projects.map((project, index) => {
-                              return (
-                                <ProjectCard
-                                  key={index}
-                                  {...project}
-                                  />
-                              )
-                            })
-                          }
-                        </Row>
-                      </Tab.Pane>
-                      <Tab.Pane eventKey="section">
+const projects = [
+  {       title: "Business Startup",
+  description: "Design & Development",
+  imgUrl: projImg1,},
+  {title: "Business Startup",
+  description: "Design & Development",
+  imgUrl: projImg2, },
+  {title: "Business Startup",
+  description: "Design & Development",
+  imgUrl: projImg3, },
+  { title: "Business Startup",
+  description: "Design & Development",
+  imgUrl: projImg4, },
+  { title: "Business Startup",
+  description: "Design & Development",
+  imgUrl: projImg5, },
+  {title: "Business Startup",
+  description: "Design & Development",
+  imgUrl: projImg6, },
+];
+
+
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>Please see my projects below</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                       </Tab.Pane>
                       <Tab.Pane eventKey="third">
@@ -99,6 +90,6 @@ export const Projects = () => {
     )
   }
 
-
   export default Projects;
+
 
